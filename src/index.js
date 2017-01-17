5253e7198afbd93bde81456b8c269070c0b23f22
Rishi Karri <rishikarri1@gmail.com>
2017-01-17 09:20:26 -0500

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// we need to bring in the router and the route from the react-router node module
+
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
